ad9a1048f41382eb99f395d28bf81f3ea1e15089
Bari <134767175+Khroxx@users.noreply.github.com>
2024-06-20 12:39:58 +0200

    message anzeige im admin panel

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message
 
+class MessageAdmin(admin.ModelAdmin):
+    field = ('text', 'created_at', 'author', 'reciever')
+    list_display = ('text', 'author', 'reciever', 'created_at')
+    search_fields = ('text', 'author')
+    pass
 # Register your models here.
+
+admin.site.register(Message, MessageAdmin)
