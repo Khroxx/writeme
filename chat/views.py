9082a7831c4dba5738be1e1c7356d1e3ddaf3f58
Bari <134767175+Khroxx@users.noreply.github.com>
2024-06-21 22:08:28 +0200

    db erstellen und nachricht anzeigen

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Chat, Message
 
 # Create your views here.
 
 def index(request):
     if request.method == 'POST':
         print('Recived Data ' + request.POST['textmessage'])
-    return render(request, 'chat/index.html', {'username': 'Bari'})
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, reciever=request.user)
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
