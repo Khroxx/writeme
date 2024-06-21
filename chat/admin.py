8c26993453db4a55bb41c48ce6632a2eb192654a
Bari <134767175+Khroxx@users.noreply.github.com>
2024-06-21 12:21:27 +0200

    09

--- chat/admin.py
+++ chat/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from .models import Message
+from .models import Chat, Message
 
 class MessageAdmin(admin.ModelAdmin):
-    field = ('text', 'created_at', 'author', 'reciever')
-    list_display = ('text', 'author', 'reciever', 'created_at')
+    field = ('chat', 'text', 'created_at', 'author', 'reciever')
+    list_display = ('text', 'chat', 'author', 'reciever', 'created_at')
     search_fields = ('text', 'author')
     pass
+
+class ChatAdmin(admin.ModelAdmin):
+    field = ('created_at',)
+    list_display = ('created_at',)
+    pass
+
+
 # Register your models here.
 
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat, ChatAdmin)
--- chat/views.py
+++ chat/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        print('Recived Data ' + request.POST['textmessage'])
     return render(request, 'chat/index.html', {'username': 'Bari'})
